26cf79ee1d3347241b5064fcddd2bf3821d6dbc9
Antonio Fernandes <imbitubadigital@gmail.com>
2025-08-16 11:22:34 -0300

    feat: re deploy prisma 2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,25 @@
 import { PrismaClient } from '@prisma/client'
 
 declare global {
-    // eslint-disable-next-line no-var
-    var prisma: PrismaClient | undefined
+	// eslint-disable-next-line no-var
+	var prisma: PrismaClient | undefined
 }
 
-export const db = global.prisma || new PrismaClient()
+const prismaClientSingleton = () => {
+	return new PrismaClient({
+		log: ['query', 'error', 'warn'],
+	})
+}
+
+export const db = global.prisma || prismaClientSingleton()
+
 if (process.env.NODE_ENV !== 'production') global.prisma = db
+
+// Adicione um tratamento de erro básico
+db.$connect()
+	.then(() => {
+		console.log('Successfully connected to database')
+	})
+	.catch((e) => {
+		console.error('Failed to connect to database:', e)
+	})
