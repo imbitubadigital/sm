613027ad0aa2e81c6bb85c624024a95348346206
Antonio Fernandes <imbitubadigital@gmail.com>
2025-09-16 18:42:13 -0300

    feat: remove space

--- src/server/signin.ts
+++ src/server/signin.ts
@@ -1,8 +1,6 @@
 'use server'
 import type { SignInProps } from '@/@types/login'
-
 import { TOKEN_KEY } from '@/middleware'
-
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
