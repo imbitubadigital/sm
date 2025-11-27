5377bb59940510135d8c1bddb56e6b9378d4e8dd
Antonio Fernandes <imbitubadigital@gmail.com>
2025-11-27 10:51:45 -0300

    feat: pixel tiktok 2

--- src/components/tiktok/index.tsx
+++ src/components/tiktok/index.tsx
@@ -1,21 +1,22 @@
 'use client'
-import { useEffect } from 'react'
+import Script from 'next/script'
 
 export function TikTokPixel() {
-	useEffect(() => {
-		if (window.ttq) return
-
-		const script = document.createElement('script')
-		script.src =
-			'https://analytics.tiktok.com/i18n/pixel/events.js?sdkid=D4K4LD3C77U3F3CUAS4G&lib=ttq'
-		script.async = true
-		script.onload = () => {
-			if (window.ttq) {
-				window.ttq.page()
-			}
-		}
-		document.head.appendChild(script)
-	}, [])
-
-	return null
+	return (
+		<>
+			<Script
+				id="tiktok-pixel-loader"
+				strategy="afterInteractive"
+				src="https://analytics.tiktok.com/i18n/pixel/events.js?sdkid=D4K4LD3C77U3F3CUAS4G&lib=ttq"
+			/>
+			<Script id="tiktok-pixel-init" strategy="afterInteractive">
+				{`
+                    if (window.ttq && typeof window.ttq.page === 'function') {
+                        window.ttq.load('D4K4LD3C77U3F3CUAS4G');
+                        window.ttq.page();
+                    }
+                `}
+			</Script>
+		</>
+	)
 }
