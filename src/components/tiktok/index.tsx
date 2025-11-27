e7a9220bc6ca5a0d46e3a07d6fbfc8e65473468c
Antonio Fernandes <imbitubadigital@gmail.com>
2025-11-27 11:07:01 -0300

    feat: pixel tiktok 3

--- src/components/tiktok/index.tsx
+++ src/components/tiktok/index.tsx
@@ -1,3 +1,4 @@
+// filepath: /Users/drt65344/Projects/Personal/SM/surpresa-musical/src/components/tiktok/index.tsx
 'use client'
 import Script from 'next/script'
 
@@ -5,16 +6,26 @@ export function TikTokPixel() {
 	return (
 		<>
 			<Script
-				id="tiktok-pixel-loader"
-				strategy="afterInteractive"
+				id="tiktok-pixel-script"
 				src="https://analytics.tiktok.com/i18n/pixel/events.js?sdkid=D4K4LD3C77U3F3CUAS4G&lib=ttq"
+				strategy="beforeInteractive"
 			/>
-			<Script id="tiktok-pixel-init" strategy="afterInteractive">
+			<Script id="tiktok-pixel-init" strategy="beforeInteractive">
 				{`
-                    if (window.ttq && typeof window.ttq.page === 'function') {
-                        window.ttq.load('D4K4LD3C77U3F3CUAS4G');
-                        window.ttq.page();
+                    window.TiktokAnalyticsObject = 'ttq';
+                    window.ttq = window.ttq || [];
+                    window.ttq.methods = [
+                        "page","track","identify","instances","debug","on","off","once","ready","alias","group",
+                        "enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"
+                    ];
+                    window.ttq.setAndDefer = function(t,e){
+                        t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}
+                    };
+                    for(var i=0;i<window.ttq.methods.length;i++){
+                        window.ttq.setAndDefer(window.ttq,window.ttq.methods[i]);
                     }
+                    window.ttq.load && window.ttq.load('D4K4LD3C77U3F3CUAS4G');
+                    window.ttq.page && window.ttq.page();
                 `}
 			</Script>
 		</>
